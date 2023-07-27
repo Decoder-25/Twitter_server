751a54e65257050702f6b2f1d1b9b709e850b8aa
Disha <bidisha2125@gmail.com>
2023-07-27 21:06:13 +0530

    Connected to mongoDB

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+//files import
+const connectDB = require('./src/config/db')
+
 dotenv.config();
 
+//creating express app
 const app = express();
 
+//mongodb connection
+connectDB();
 
 //port
 const PORT = process.env.PORT || 6000; 
