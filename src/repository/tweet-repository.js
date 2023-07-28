e35a08416c1aa1954b9eea25c06d310a1b47bb0d
Disha <bidisha2125@gmail.com>
2023-07-28 21:43:03 +0530

    Added get all hashtags in tweet repo

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,24 +1,43 @@
 const Tweet = require('../models/tweet');
 
 class TweetRepository {
-
-    //creating the tweets
-    async create(data) {
-        try {
-            const tweet = await Tweet.create(data);
-            return tweet;
-        } catch(error) {
-            console.log(error);
-        }
+  //creating the tweets
+  async create(data) {
+    try {
+      const tweet = await Tweet.create(data);
+      return tweet;
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    //getting the tweets based on theit tweet id
-    async get(id){
-        try{
-            const tweet = await Tweet.findById(id);
-            return tweet; 
-        }catch(error){
-            console.log(error);
-        }
+  //getting the tweets based on theit tweet id
+  async get(id) {
+    try {
+      const tweet = await Tweet.findById(id);
+      return tweet;
+    } catch (error) {
+      console.log(error);
     }
+  }
+
+  //deleting the tweets
+  async destroy(id) {
+    try {
+      const tweet = await Tweet.findByIdAndRemove(id);
+      return tweet;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //getting all the hashtags
+  async getAll(offset, limit) {
+    try {
+      const tweet = await Tweet.find().skip(offset).limit(limit);
+      return tweet;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
